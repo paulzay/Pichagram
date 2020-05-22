e9c30bbfc00d82c3b4f4bdce9800055abde49af7
paulzay <paulotieno2@gmail.com>
2020-05-22 21:37:32 +0300

    finally get the upload working

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.7.1'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
 gem 'carrierwave'
 gem 'simple_form'
+gem 'bootstrap-sass'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 end
